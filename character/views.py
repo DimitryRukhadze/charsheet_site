ce0c19e1efcb8a6e0f3364f7294166e280db5e09
DimitryRukhadze <lokiinflames@gmail.com>
2023-11-01 21:46:29 +0300

    Made correct JSON serialization

--- character/views.py
+++ character/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.core import serializers
+from rest_framework.serializers import ModelSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Race, Theme
 
 
-# Create your views here.
+class RaceSerializer(ModelSerializer):
+    class Meta:
+        model = Race
+        fields = '__all__'
+
+
 def index(request):
     races = list(Race.objects.all())
     themes = list(Theme.objects.all())
@@ -16,8 +21,11 @@ def index(request):
     }
     return render(request, 'index.html', context=context)
 
+
 @api_view(['GET'])
 def races_list(request):
     races = Race.objects.all()
-    races_json = serializers.serialize('json', races)
-    return Response(races_json)
+    races_serializer = RaceSerializer(data=races, many=True)
+    races_serializer.is_valid()
+    response_data = races_serializer.data
+    return Response(response_data)
